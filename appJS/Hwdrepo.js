13c7193cda27965703321478e7d58678ab789fc8
mihil <mihil@wso2.com>
2014-02-11 18:02:53 +0530

    Revert "added datatables"
    This reverts commit 7a4a88b6625be992366dcfb4e08fb4a931222d9a.

--- appJS/Hwdrepo.js
+++ appJS/Hwdrepo.js
@@ -74,46 +74,12 @@ Hwdrepo = new function () {
     this.viewRequests = function () {
         HwdrepoUtil.makeJsonRequest("GET", controllerPath, "operation=loadRequests", function (html) {
 
-            alert(JSON.stringify(html));
-            ss=html.requests;
-            var dataArray = new Array;
-            var aaData=new Array;
-            
-            for(var i=0;i<ss.length;i++){
-
-                var dataObject=ss[i];
-
-for(var o in dataObject) {
-
-    dataArray.push(dataObject[o]);
-}
- //dataArray.push("<input type='checkbox' name='check"+dataObject['request_id']+"' value='"+dataObject['request_id']+"'>");
-
-       aaData.push(dataArray);
-       dataArray=new Array;
-}
-
-            //Hwdrepo.fillRequestTable(html);
-
-            /********************/
-        $('#example').dataTable( {
-        "aaData": aaData,
-        "aoColumns": [
-            { "sTitle": "Device Type" },
-            { "sTitle": "Purpose" },
-            { "sTitle": "Requirements" },
-            { "sTitle": "Request ID", "sClass": "center" }
-         //  { "sTitle": "Grade", "sClass": "center" }
-        ]
-    } 
-
-    ); 
-/******************************************/
+            Hwdrepo.fillRequestTable(html);
 
         });
 
     }
-/*
+
     this.fillRequestTable = function (html) {
 
 
@@ -126,8 +92,6 @@ for(var o in dataObject) {
         tbody.innerHTML = '';
 
         var obj = html;
-
-        alert(JSON.stringify(obj));
 
         for (var i = 0; i < obj.requests.length; i++) {
 
@@ -142,46 +106,19 @@ for(var o in dataObject) {
 
             for (var property in object) {
 
-                if(property=="request_id"){
 
-
-
-                    var tdCheck = document.createElement('td');
-                    var checkbox = document.createElement('input');
-                    checkbox.type = "checkbox";
-                    checkbox.id = "chkbox_"+object[property];
-
-
-                    tdCheck.appendChild(checkbox);
-                    newRow.appendChild(tdCheck);
-
-                }
-                else{
                 var td = document.createElement('td');
 
 
                 td.appendChild(document.createTextNode(object[property]));
                 newRow.appendChild(td);
 
-
-
-
             }
-        }
-
-            
-
-//var label = document.createElement('label')
-//label.htmlFor = "id";
-//
-
-//tdCheck.appendChild(label);
-            
 
 
             tbody.appendChild(newRow);
         }
-    }*/
+    }
 
     this.requestDevice = function (deviceType, purpose, requirement) {
 
@@ -571,11 +508,6 @@ for(var o in dataObject) {
             function (html) {
                  var userID=html;
             });
-    }
-    this.deleteRequests = function(cb){
-
-       alert(cb);
-
     }
 
 }
